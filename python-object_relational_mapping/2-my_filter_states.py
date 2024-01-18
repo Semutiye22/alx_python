5bcd5875ae67e45d9496e2c991db085b4db7e50b
Semira Hussien <semutiye22@gmail.com>
2024-01-18 17:01:44 +0300

    hello

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
 import sys
+import MySQLdb
 
-# Extract command line arguments
-mysql_user = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
+if name == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-# Connect to MySQL server
-db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user, passwd=mysql_password, db=database_name)
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
-# Create a cursor object to interact with the database
-cursor = db.cursor()
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Query states with the provided state name and display the results
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
 
 
 
