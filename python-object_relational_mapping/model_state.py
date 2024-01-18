639e2b1094016c7a3cb65f1595f967fbdb3f70ec
Semira Hussien <semutiye22@gmail.com>
2024-01-18 17:40:29 +0300

    hi

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""
+Start link class to table in database
 """
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class State(Base):
-    """Class definition of a State"""
+    """State class inherits from Base"""
 
     tablename = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 if name == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract arguments
+    username, password, database = sys.argv[1:]
+
+    # Create engine and link to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database),
+                           pool_pre_ping=True)
+
+    # Create all tables in the database
     Base.metadata.create_all(engine)
