0a0f7d47d632c146de118cc9ed3fafeb7f546f43
Semira Hussien <semutiye22@gmail.com>
2024-01-24 11:29:55 +0300

    hello

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,9 +1,33 @@
+"""
+0-hello_route.py - Simple Flask web application.
+
+This script creates a basic Flask web application with a single route that
+responds with "Hello HBNB!" when accessed at the root ("/") route.
+
+Usage:
+    - Run the script using 'python3 0-hello_route.py'.
+    - The web application will be accessible at http://0.0.0.0:5000/.
+
+Routes:
+    - /: Displays "Hello HBNB!"
+
+Requirements:
+    - Flask: Make sure to install Flask using 'pip install Flask'.
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route function for the root ("/") route.
+
+    Returns:
+        str: The string "Hello HBNB!" as the response.
+    """
     return 'Hello HBNB!'
 
 if name == '__main__':
